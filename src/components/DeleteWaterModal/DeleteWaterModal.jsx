e412d9ab66236e4d91ce205aa17551c23b061cc0
fulaytar <fulaytar2405@gmail.com>
2024-08-03 00:53:02 +0300

    need add tablet+desktop

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,5 +1,20 @@
+import css from './DeleteWaterModal.module.css';
+
 const DeleteWaterModal = () => {
-  return <></>;
+  return (
+    <div className={css.container}>
+      <h2 className={css.h2}>Delete entry</h2>
+      <p className={css.p}>Are you sure you want to delete the entry?</p>
+      <div className={css.containerBtn}>
+        <button type='button' className={css.delete}>
+          Delete
+        </button>
+        <button type='button' className={css.cancel}>
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteWaterModal;
