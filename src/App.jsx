18fdf87b59502fb1c53a5a0d978b311150ae9796
fulaytar <fulaytar2405@gmail.com>
2024-08-05 20:28:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PrivateRoute from './components/permissions/PrivateRoute';
 const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
 //const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage.jsx"));
 // const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage.jsx"));
-//const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
+const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
 //const NotFoundPage = lazy(() =>
 // import("../pages/NotFoundPage/NotFoundPage.jsx"));
 
@@ -16,6 +16,7 @@ function App() {
     <SharedLayout>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/tracker' element={<TrackerPage />} />
       </Routes>
     </SharedLayout>
   );
