a1741c2c94279de5be6ed657949269572f9fa54d
fulaytar <fulaytar2405@gmail.com>
2024-08-05 20:29:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import SharedLayout from './components/SharedLayout/SharedLayout';
-import { lazy } from 'react';
-import RestrictedRoute from './components/permissions/RestrictedRoute';
-import PrivateRoute from './components/permissions/PrivateRoute';
+import { Route, Routes } from "react-router-dom";
+import SharedLayout from "./components/SharedLayout/SharedLayout";
+import { lazy } from "react";
+import RestrictedRoute from "./components/permissions/RestrictedRoute";
+import PrivateRoute from "./components/permissions/PrivateRoute";
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
 //const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage.jsx"));
 // const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage.jsx"));
-const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
+const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
 //const NotFoundPage = lazy(() =>
 // import("../pages/NotFoundPage/NotFoundPage.jsx"));
 
@@ -15,8 +15,8 @@ function App() {
   return (
     <SharedLayout>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/tracker' element={<TrackerPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tracker" element={<TrackerPage />} />
       </Routes>
     </SharedLayout>
   );
