297cbb32ce50c040f9981dcf614694df1e6be90b
Dmytro <az.36419.a@gmail.com>
2024-08-06 22:56:22 +0300

    visual done

---
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -0,0 +1,11 @@
+import UserSettingsForm from "../UserSettingsForm/UserSettingsForm";
+import css from "./UserSettingsModal.module.css";
+
+export default function UserSettingsModal() {
+  return (
+    <div className={css.modal}>
+      <h1 className={css.title}>Setting</h1>
+      <UserSettingsForm />
+    </div>
+  );
+}
