1448b1bd89fb25e734a7a7216921914448e92002
oleksasa <oleksandrasevcuk04@gmail.com>
2024-08-09 15:55:12 +0300

    add logOut and not found Page

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
 const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
 const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
 const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
-//const NotFoundPage = lazy(() =>
-// import("../pages/NotFoundPage/NotFoundPage.jsx"));
+const NotFoundPage = lazy(() =>
+  import('./pages/NotFoundPage/NotFoundPage.jsx'),
+);
 
 function App() {
   return (
@@ -39,6 +40,7 @@ function App() {
             <PrivateRoute redirectTo="/signin" component={<TrackerPage />} />
           }
         />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </SharedLayout>
   );
