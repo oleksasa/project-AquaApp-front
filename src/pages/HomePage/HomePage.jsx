1b1f43a586ee7097b56fed83299965fba104000e
oleksasa <oleksandrasevcuk04@gmail.com>
2024-08-10 20:02:04 +0300

    first commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,23 @@
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
-import Logo from '../../components/Logo/Logo';
 import css from './HomePage.module.css';
+import { Layout } from '../../components/layouts/Layout/Layout';
 
 export default function HomePage() {
   return (
-    <div className={css.home}>
-      <Logo />
-      <WelcomeSection />
-      <AdvantagesSection />
-    </div>
+    <Layout
+      leftComponent={{
+        component: (
+          <div className={css.home}>
+            <WelcomeSection />
+          </div>
+        ),
+        bg: 'var(--color-main-bg)',
+      }}
+      rightComponent={{
+        component: <AdvantagesSection />,
+        bg: 'var(--color-main-bg)',
+      }}
+    />
   );
 }
