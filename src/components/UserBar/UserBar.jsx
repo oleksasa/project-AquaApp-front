4d50d9f163d2e4980d59d57630bd64de5e8a2490
Sulym Dmytro <ds.intelserv@gmail.com>
2024-08-10 11:27:14 +0300

    Fixed user slice

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -3,12 +3,16 @@ import Icon from '../Icon/Icon';
 import { IoPersonCircleSharp } from 'react-icons/io5';
 import UserBarPopover from '../UserBarPopover/UserBarPopover';
 import css from './UserBar.module.css';
+import { useSelector } from 'react-redux';
+import { selectUserName } from '../../redux/auth/selectors.js';
 
 export default function UserBar() {
   const [isShow, setIsShow] = useState(false);
   const [popoverWidth, setPopoverWidth] = useState(null);
   const buttonRef = useRef(null);
   const popoverRef = useRef(null);
+
+  const userName  = useSelector(selectUserName);
 
   const handlClick = () => {
     if (!isShow) {
@@ -50,7 +54,7 @@ export default function UserBar() {
         type="button"
         onClick={handlClick}
       >
-        User name
+        {userName ? userName : "User"}
         <div className={css.wrapAvatar}>
           <IoPersonCircleSharp className={css.avatar} />
         </div>
