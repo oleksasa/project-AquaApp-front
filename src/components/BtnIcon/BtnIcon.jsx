a33c4c24cb08a0ec17560b9620b27fa6ae0c3de0
oleksandraburymenko <olexandralife@gmail.com>
2024-08-11 18:21:40 +0300

    change id

--- src/components/BtnIcon/BtnIcon.jsx
+++ src/components/BtnIcon/BtnIcon.jsx
@@ -2,7 +2,7 @@ import icons from "../../../public/symbol-defs.svg";
 import css from "./BtnIcon.module.css";
 
 export default function BtnIcon({ id, className }) {
-  const iconClass = (id === 'pie-chart-01' || id === 'pie-chart-02') ? css['icon-statistics'] : css['icon-pagination'];
+  const iconClass = (id === 'icon-pie-chart-01' || id === 'icon-pie-chart-02') ? css['icon-statistics'] : css['icon-pagination'];
     return (
       <svg className={`${className} ${iconClass}`} >
         <use href={`${icons}#${id}`}/>
