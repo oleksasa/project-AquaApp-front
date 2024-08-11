f1370bae45bc9f12fa3871327cfafbca4139864f
oleksandraburymenko <olexandralife@gmail.com>
2024-08-12 00:52:16 +0300

    change

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,35 +1,38 @@
-
-import { useSelector } from 'react-redux';
-import { format, eachDayOfInterval, startOfMonth, endOfMonth, startOfDay } from "date-fns";
+import { eachDayOfInterval, startOfMonth, endOfMonth, startOfDay } from "date-fns";
 import CalendarItem from '../CalendarItem/CalendarItem';
 import css from './Calendar.module.css';
-import { selectTotalWaterPerDay } from '../../redux/water/selectors.js';
+import { useSelector } from "react-redux";
+import { selectTotalWaterPerDay } from "../../redux/water/selectors";
 
-const Calendar = ({ currentMonth, onDayClick }) => {
+
+
+const Calendar = ({ currentMonth, onDayClick, selectedDay }) => {
+    
     const dailyWaterData = useSelector(selectTotalWaterPerDay);
+    console.log('Daily Water Data:', dailyWaterData);
 
     const start = startOfMonth(currentMonth);
     const end = endOfMonth(currentMonth);
     const days = eachDayOfInterval({ start, end });
     
-    const today = startOfDay(new Date());
 
     return (
         <div className={css.container}>
             {days.map(day => {
-                const formattedDate = format(day, 'yyyy-MM-dd');
-                const dailyWaterIntake = dailyWaterData && dailyWaterData[formattedDate]
-                ? dailyWaterData[formattedDate].dailyWaterIntake || 0
-                : 0;
+                // const formattedDate = format(day, 'yyyy-MM-dd');
+                // const dailyWaterIntake = dailyWaterData[formattedDate]
+                // ? dailyWaterData[formattedDate].dailyWaterIntake || 0
+                // : 0;
+                // console.log(`Formatted Date: ${formattedDate}, Intake: ${dailyWaterIntake}`);
 
-                const isSelected = startOfDay(day).getTime() === today.getTime();
+                const isSelected = startOfDay(day).getTime() === startOfDay(selectedDay).getTime();
             
             return (
                 <CalendarItem
                 key={day.toISOString()}
                 date={day}
                 onClick={onDayClick}
-                dailyWaterIntake={dailyWaterIntake}
+                dailyWaterIntake={dailyWaterData}
                 isSelected={isSelected}
                 />
             );
