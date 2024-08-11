22d2ddf214d48090fcf2b1ce846da9cbb134ff90
Alina Alieks <alieksieienko.alina@gmail.com>
2024-08-11 12:44:47 +0300

    UserBarPopover add modals

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,8 +1,8 @@
-import UserPanel from "../UserPanel/UserPanel";
-import DailyInfo from "../DailyInfo/DailyInfo";
-import MonthInfo from "../MonthInfo/MonthInfo";
+import UserPanel from '../UserPanel/UserPanel';
+import DailyInfo from '../DailyInfo/DailyInfo';
+import MonthInfo from '../MonthInfo/MonthInfo';
 
-import css from "./WaterDetailedInfo.module.css";
+import css from './WaterDetailedInfo.module.css';
 
 export default function WaterDetailedInfo() {
   return (
