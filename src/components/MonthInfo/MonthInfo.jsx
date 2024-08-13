5e71a03c6b8c20f1cad7ddd706836830ec1a1617
Sulym Dmytro <ds.intelserv@gmail.com>
2024-08-13 19:34:51 +0300

    Fixed-calendar

--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -19,9 +19,6 @@ const MonthInfo = () => {
 
     const rawMonthlyData = useSelector(selectTotalWaterPerMonth) || [];
 
-
-    console.log('Formatted Monthly Water Data:', rawMonthlyData);
-
     const selectedDayFromRedux = useSelector(selectChoosingDay);
     
 
